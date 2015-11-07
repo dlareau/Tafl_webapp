f04adec3f4994d88778a44e2bd8003a7981ff8cd
Jane Threefoot <jane.threefoot@gmail.com>
2015-11-07 17:34:24 -0500

    basic html for loginregister page done, though the forms stuff will probs need adjusting. fixed urls.py login entry to match our actual setup.

--- webapps/tafl/urls.py
+++ webapps/tafl/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 urlpatterns = [
     url(r'^$', 'tafl.views.gamespage', name='games'),
     url(r'^register$', 'tafl.views.register', name='register'),
-    url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'tafl/login.html'}),
+    url(r'^login$', 'tafl.views.login', name='login'),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
     url(r'^makegame$', 'tafl.views.makegame', name='makegame'),
     url(r'^joingame$', 'tafl.views.joingame', name='joingame'),
