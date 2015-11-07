fddf53188fc4f9e7a8e2555a7a27e998fbac9452
Jane Threefoot <jane.threefoot@gmail.com>
2015-11-06 19:48:52 -0500

    filled out urls, put corresponding stubs in views

--- webapps/tafl/urls.py
+++ webapps/tafl/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$', 'tafl.views.gamespage', name='games'),
+    url(r'^register$', 'tafl.views.register', name='register'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'tafl/login.html'}),
-    url(r'^logout%', 'django.contrib.auth.views.logout_then_login', name='logout'),
-    #but wait there's more :O
+    url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
+    url(r'^makegame$', 'tafl.views.makegame', name='makegame'),
+    url(r'^joingame$', 'tafl.views.joingame', name='joingame'),
+    url(r'^profile', 'tafl.views.profile', name='view'),
 ]
--- webapps/tafl/views.py
+++ webapps/tafl/views.py
@@ -4,3 +4,15 @@ from ws4redis.redis_store import RedisMessage
 
 def gamespage(request):
     return render(request, "tafl/mainpage.html")
+
+def makegame(request):
+    return None
+
+def joingame(request):
+    return None
+
+def profile(request):
+    return None
+
+def register(request):
+    return None
