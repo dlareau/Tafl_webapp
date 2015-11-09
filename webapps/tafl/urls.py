30cc327c13280909893d3ae84f5b8afda094c1ef
Dillon Lareau <flybye22@gmail.com>
2015-11-08 19:34:48 -0500

    Basic game page routing and views

--- webapps/tafl/urls.py
+++ webapps/tafl/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 
 urlpatterns = [
     url(r'^$', 'tafl.views.gamespage', name='games'),
+    url(r'^game$', 'tafl.views.game', name='game'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'tafl/login.html'}),
     url(r'^logout%', 'django.contrib.auth.views.logout_then_login', name='logout'),
     #but wait there's more :O
--- webapps/tafl/views.py
+++ webapps/tafl/views.py
@@ -4,3 +4,6 @@ from ws4redis.redis_store import RedisMessage
 
 def gamespage(request):
     return render(request, "tafl/mainpage.html")
+
+def game(request):
+    return render(request, "tafl/gamepage.html")
